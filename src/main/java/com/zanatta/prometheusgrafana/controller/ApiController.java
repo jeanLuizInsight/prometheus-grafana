058c49dac1c55ea09b0e0a2ac989742e57453cd0
jl.zanatta92@yahoo.com.br <jl.zanatta92@yahoo.com.br>
2022-01-04 21:42:09 -0300

    Spring initializer.

---
+++ src/main/java/com/zanatta/prometheusgrafana/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.zanatta.prometheusgrafana.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller API para realização de testes
+ * @author <a href="mailto:jean.zanatta@unoesc.edu.br">Jean Luiz Zanatta</a>
+ * @since 04/01/2022
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+}
